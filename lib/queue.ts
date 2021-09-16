c729b4f4ee43adb45038eac509e1324c2976a828
s-beats <sakashin10291029@gmail.com>
2021-09-16 22:39:22 +0900

    fix: import

--- lib/queue.ts
+++ lib/queue.ts
@@ -1,9 +1,5 @@
 import * as cdk from "@aws-cdk/core";
-import * as lambda from "@aws-cdk/aws-lambda";
-import * as ecr from "@aws-cdk/aws-ecr";
 import * as sqs from "@aws-cdk/aws-sqs";
-import { SqsEventSource } from '@aws-cdk/aws-lambda-event-sources';
-import { Role, ServicePrincipal, ManagedPolicy } from "@aws-cdk/aws-iam";
 
 export class QueueStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {

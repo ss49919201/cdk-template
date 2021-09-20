8384c083af510f9eceeb325b64941c3cf51e0c49
s-beats <sakashin10291029@gmail.com>
2021-09-20 22:13:51 +0900

    lambdaスタックの命名変更

--- bin/cdk-sample.ts
+++ bin/cdk-sample.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { LambdaStack } from '../lib/lambda';
+import { LambdaFunctionStack } from '../lib/lambda';
 import { QueueStack } from '../lib/queue';
 import { ECRStack } from '../lib/ecr';
 
 const env = { account: process.env.ACCOUNT, region: process.env.REGION }
 const app = new cdk.App();
-new LambdaStack(app, 'LambdaStack', { env });
+new LambdaFunctionStack(app, 'LambdaFunctionStack', { env });
 new QueueStack(app, 'QueueStack', { env });
 new ECRStack(app, 'ECRStack', { env });
--- lib/lambda.ts
+++ lib/lambda.ts
@@ -4,10 +4,9 @@ import * as ecr from "@aws-cdk/aws-ecr";
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as sqs from "@aws-cdk/aws-sqs";
 import { SqsEventSource } from '@aws-cdk/aws-lambda-event-sources';
-import { StringParameter } from '@aws-cdk/aws-ssm';
 import { SecretValue } from "@aws-cdk/core";
 
-export class LambdaStack extends cdk.Stack {
+export class LambdaFunctionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     // ECRリポジトリをARN経由で参照
@@ -35,7 +34,7 @@ export class LambdaStack extends cdk.Stack {
     // NOTE: 環境変数はSecretsManager,SSMから取得(管理を一元化、CFnテンプレートに値をハードコーディングしない)
     // https://docs.aws.amazon.com/ja_jp/lambda/latest/dg/configuration-envvars.html#configuration-envvars-samples
     // https://dev.classmethod.jp/articles/cdk-approval-pipeline/
-    const func = new lambda.DockerImageFunction(this, "Lambda", {
+    const fn = new lambda.DockerImageFunction(this, "Lambda", {
       code: lambda.DockerImageCode.fromEcr(repository, {}),
       functionName: "lambda",
       environment: {
@@ -44,10 +43,15 @@ export class LambdaStack extends cdk.Stack {
         "PROTOCOL": SecretValue.secretsManager("PROTOCOL").toString(),
         "DB_HOST": SecretValue.secretsManager("DB_HOST").toString(),
         "DB_NAME": SecretValue.secretsManager("DB_NAME").toString(),
-        "ID": StringParameter.valueForStringParameter(this, "ID"),
       },
       events: [source],
       vpc: vpc,
     });
+
+    // Export
+    new cdk.CfnOutput(this, "lambda-arn", {
+      value: fn.functionArn,
+      exportName: "lambda-arn",
+    })
   }
 }

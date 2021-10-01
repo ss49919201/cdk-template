2bf69c88fd452e6be010b0ef656a7607066a33c1
s-beats <sakashin10291029@gmail.com>
2021-10-02 00:55:51 +0900

    quote

--- lib/lambda.ts
+++ lib/lambda.ts
@@ -1,8 +1,8 @@
-import * as cdk from "@aws-cdk/core";
-import * as lambda from "@aws-cdk/aws-lambda";
-import * as ecr from "@aws-cdk/aws-ecr";
-import * as ec2 from "@aws-cdk/aws-ec2";
-import * as sqs from "@aws-cdk/aws-sqs";
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as ecr from '@aws-cdk/aws-ecr';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as sqs from '@aws-cdk/aws-sqs';
 import { SqsEventSource } from '@aws-cdk/aws-lambda-event-sources';
 
 export class LambdaFunctionStack extends cdk.Stack {
@@ -13,41 +13,41 @@ export class LambdaFunctionStack extends cdk.Stack {
     // https://github.com/aws/aws-cdk/blob/efe5112a2c6ae5e70b2631711b95b04d10062812/packages/%40aws-cdk/aws-ecr/lib/repository.ts#L395
     const repository = ecr.Repository.fromRepositoryAttributes(
       this,
-      "repository",
+      'repository',
       {
-        repositoryName: cdk.Fn.importValue("repository-name"),
-        repositoryArn: cdk.Fn.importValue("repository-arn"),
+        repositoryName: cdk.Fn.importValue('repository-name'),
+        repositoryArn: cdk.Fn.importValue('repository-arn'),
       }
     );
 
     // イベントソースとなるQueue
-    const queue = sqs.Queue.fromQueueArn(this, "queue", cdk.Fn.importValue("queue-arn"))
+    const queue = sqs.Queue.fromQueueArn(this, 'queue', cdk.Fn.importValue('queue-arn'))
     const source = new SqsEventSource(queue)
 
     // LambdaをデプロイするVPC
-    const vpc = ec2.Vpc.fromLookup(this, "vpc", {
-      vpcId: this.node.tryGetContext("vpcID"),
+    const vpc = ec2.Vpc.fromLookup(this, 'vpc', {
+      vpcId: this.node.tryGetContext('vpcID'),
     })
 
     // デプロイ時にはイメージを置いておく必要があるので注意
     // NOTE: 環境変数はSecretsManager,SSMから取得(管理を一元化、CFnテンプレートに値をハードコーディングしない)
     // https://docs.aws.amazon.com/ja_jp/lambda/latest/dg/configuration-envvars.html#configuration-envvars-samples
     // https://dev.classmethod.jp/articles/cdk-approval-pipeline/
-    const fn = new lambda.DockerImageFunction(this, "Lambda", {
+    const fn = new lambda.DockerImageFunction(this, 'Lambda', {
       code: lambda.DockerImageCode.fromEcr(repository, {}),
-      functionName: "function",
+      functionName: 'function',
       events: [source],
       vpc: vpc,
     });
 
     // Export
-    new cdk.CfnOutput(this, "lambda-arn", {
+    new cdk.CfnOutput(this, 'lambda-arn', {
       value: fn.functionArn,
-      exportName: "lambda-arn",
+      exportName: 'lambda-arn',
     })
-    new cdk.CfnOutput(this, "lambda-name", {
+    new cdk.CfnOutput(this, 'lambda-name', {
       value: fn.functionName,
-      exportName: "lambda-name",
+      exportName: 'lambda-name',
     })
   }
 }
